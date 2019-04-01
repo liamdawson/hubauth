2e69de1bf249982b74da4064b6f030b4be9881e3
Liam Dawson <liam@ldaws.com>
2019-04-01 16:41:29 +1100

    fmt/clippy

--- doc/man/config.rs
+++ doc/man/config.rs
@@ -1,10 +1,16 @@
+use crate::doc::bin_constants::{default_cache, default_config};
 use man::prelude::*;
-use crate::doc::bin_constants::{default_config, default_cache};
 
 pub fn generate() -> Vec<(&'static str, String)> {
-    vec![("hubauth.yml.5",
-    Manual::new("hubauth.yml")
-        .description(format!("{}", default_config()))
-        .option(Opt::new("caching-destination").long("cache.destination").default_value(default_cache()))
-        .render())]
+    vec![(
+        "hubauth.yml.5",
+        Manual::new("hubauth.yml")
+            .description(format!("{}", default_config()))
+            .option(
+                Opt::new("caching-destination")
+                    .long("cache.destination")
+                    .default_value(default_cache()),
+            )
+            .render(),
+    )]
 }
