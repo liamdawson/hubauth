2fb83ce5991bffc7737667768ed4fac6b8d664ae
Liam Dawson <liam@ldaws.com>
2019-04-04 16:19:43 +1100

    add missing build packages

--- misc/vagrant/machine-config.sh
+++ misc/vagrant/machine-config.sh
@@ -6,7 +6,12 @@ apt-get update -y
 apt-get install -y \
   build-essential \
   pbuilder \
-  git-buildpackage
+  git-buildpackage \
+  libssl-dev \
+  libssl1.1 \
+  pkg-config
+
+pbuilder create
 
 pbuilder --create --distribution bionic --architecture amd64 --basetgz /var/cache/pbuilder/bionic-amd64-base.tgz
 pbuilder --create --distribution cosmic --architecture amd64 --basetgz /var/cache/pbuilder/cosmic-amd64-base.tgz
